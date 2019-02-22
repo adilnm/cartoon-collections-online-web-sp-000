0c8783089b027927ee0363f583aa874fea13db89
ADIL NAIMI <adilnm23@hotmail.com>
2019-02-22 05:24:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-  array.map{}
+  array.map{|planteer| planteer.capita}
 end
 
 def long_planeteer_calls# code an argument here
