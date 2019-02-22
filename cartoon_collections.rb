cb2924011b3223bbd536eb7be34a89d47781c003
ADIL NAIMI <adilnm23@hotmail.com>
2019-02-22 05:40:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,7 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.each do |word|
-    return true if(word.length>4)
+  array.any?{|word| word.length>4}
 
   
 end
